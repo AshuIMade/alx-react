17bc25c9f6690946ef7b5056308091796df35aed
Ashenafi Yakob <asheimade@gmail.com>
2023-08-25 15:36:48 +0300

    add notifification and close icon task 2 #4

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -4,3 +4,7 @@ export const getFullYear = () => {
 export const getFooterCopy = () => {
   return `© ${getFullYear()} Holberton School main dashboard`;
 };
+
+export const getLatestNotification = () => {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+};
